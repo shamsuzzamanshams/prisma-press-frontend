b270d7cc7424b202a2d54980864618e7eebcaa1c
shamsuzzamanshams <shams30200@gmail.com>
2026-07-18 22:33:21 +0600

    install shadcn

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div>Hello Next.Js</div>
+    <div>Hello Next.Js
+
+      <Button
+        size={"xs"}
+        variant={"destructive"}
+      >
+        Click Me
+      </Button>
+
+
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 
 export default function RootLayout({
@@ -11,7 +14,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>

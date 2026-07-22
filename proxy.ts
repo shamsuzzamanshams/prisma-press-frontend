18a5f1056d70e2671cdc5dd7cc0202205fed7727
shamsuzzamanshams <shams30200@gmail.com>
2026-07-22 16:51:42 +0600

    role based authorized

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import jwt, { JwtPayload } from "jsonwebtoken"
+import { jwtUtils } from './utils/jwt';
+import { AwardIcon } from 'lucide-react';
+import { cookies } from 'next/headers';
 
 // This function can be marked `async` if using `await` inside
 
@@ -8,20 +11,27 @@ const AUTH_ROUTE = ["/login", "/register"];
 const PUBLIC_ROUTE = ["/", "/new"];
 export async function proxy(request: NextRequest) {
 	const pathName = request.nextUrl.pathname;
-	console.log(request.nextUrl, "Requst");
-	console.log(pathName, "pathName");
+	// console.log(request.nextUrl, "Requst");
+	// console.log(pathName, "pathName");
+
+	const cookieStore = await cookies();
 
 
-	console.log("Proxy");
+	// console.log("Proxy");
 
 	const accessToken = request.cookies.get("accessToken")?.value
 
-	const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null
+	const decodedToken = accessToken ? jwtUtils.verifiToken(accessToken, process.env.JWT_ACCESS_SECRET as string) : null
 
 	let userRole = null;
 
-	if (decodedToken) {
-		userRole = decodedToken.role;
+	if(!decodedToken?.success){
+		cookieStore.delete("accessToken");
+		return NextResponse.redirect(new URL('/login', request.url));
+	}
+
+	if (decodedToken?.success && decodedToken.data) {
+		userRole = (decodedToken.data as JwtPayload).role;
 	}
 
 	if (accessToken && AUTH_ROUTE.includes(pathName)) {
@@ -46,8 +56,18 @@ export async function proxy(request: NextRequest) {
 	if (!accessToken && !isPublic && !isAuth) {
 		return NextResponse.redirect(new URL('/login', request.url));
 	}
+
+	if (pathName.startsWith("/dashboard") && userRole !== "USER") {
+		return NextResponse.redirect(new URL('/not-found', request.url));
+	}
+	else if (pathName.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+		return NextResponse.redirect(new URL('/not-found', request.url));
+	}
+	else if (pathName.startsWith("/author-dashboard") && userRole !== "AUTHOR") {
+		return NextResponse.redirect(new URL('/not-found', request.url));
+	}
 	// return NextResponse.redirect(new URL('/', request.url))
-	return NextResponse.next()
+	return NextResponse.next();
 
 
 }
---
+++ utils/jwt.ts
@@ -0,0 +1,38 @@
+"use server"
+
+
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+
+// const createToken = (payload: JwtPayload, secret: string, expiresIn: SignOptions) => {
+// 	const token = jwt.sign(
+// 		payload,
+// 		secret,
+// 		{
+// 			expiresIn
+// 		} as SignOptions);
+// 	return token;
+// };
+
+const verifiToken = (token: string, secret: string) => {
+	try {
+		const verifiedToken = jwt.verify(token, secret);
+		return {
+			success: true,
+			data: verifiedToken
+		}
+	} catch (error: any) {
+		console.log("Token Verification Faield", error);
+
+		return {
+			success: false,
+			error: error.message
+		}
+	}
+}
+
+export const jwtUtils = {
+	// createToken,
+	verifiToken,
+};
